382a2e9ed5c22f472d2867f959e13bcaa6c3a79b
Neto test <s.e.lebedeva74@gmail.com>
2023-12-09 18:03:40 +0500

    Refotmated code

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -34,35 +34,36 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void shouldCalculateForUnregisteredAndUnderLimit() {
-            BonusService service = new BonusService();
+        BonusService service = new BonusService();
 
-            // подготавливаем данные:
-            long amount = 2000;
-            boolean registered = false;
-            long expected = 20;
+        // подготавливаем данные:
+        long amount = 2000;
+        boolean registered = false;
+        long expected = 20;
 
-            // вызываем целевой метод:
-            long actual = service.calculate(amount, registered);
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
 
-            // производим проверку (сравниваем ожидаемый и фактический):
-            Assertions.assertEquals(expected, actual);
-        }
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
 
     @Test
     void shouldCalculateForUnregisteredAndOverLimit() {
-            BonusService service = new BonusService();
+        BonusService service = new BonusService();
 
-            // подготавливаем данные:
-            long amount = 55_000;
-            boolean registered = false;
-            long expected = 500;
+        // подготавливаем данные:
+        long amount = 55_000;
+        boolean registered = false;
+        long expected = 500;
 
-            // вызываем целевой метод:
-            long actual = service.calculate(amount, registered);
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
 
-            // производим проверку (сравниваем ожидаемый и фактический):
-            Assertions.assertEquals(expected, actual);
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
     }
 }
